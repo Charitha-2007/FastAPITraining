8357583bbb0f4983f2a65343a8f8e8c2cd544a01
Charitha-2007 <2025cs_dcharitha_b@nie.ac.in>
2026-09-22 14:42:03 +0530

    Path parameters with type hint

--- Day/Day2/FAstAPIPractice/main.py
+++ Day/Day2/FAstAPIPractice/main.py
@@ -16,3 +16,13 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message":"Created"}
+
+#Path Parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return{"Result":"Distinction","usn":usn}
+
+#Path Parameters with type hint
+@app.get("/candidate/{rollno}")
+def get_result(rollno:int):
+    return{"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
